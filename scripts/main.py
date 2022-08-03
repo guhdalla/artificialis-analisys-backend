0989be8d74984121dc512a1f9688061d85e75aec
guhdalla <guhdalla011@gmail.com>
2022-08-03 12:08:56 -0300

    FEAT: Second Page

--- scripts/main.py
+++ scripts/main.py
@@ -9,6 +9,7 @@ from routers import health_check
 from routers import pesquisa_router
 from routers import nps_router
 from routers import analisar_router
+from routers import palavra_chave_router
 
 application_name = "artificialis-analisys-backend"
 application_description = "Aplicacao para analise textual"
@@ -28,3 +29,4 @@ app.include_router(health_check.router, tags=["App"])
 app.include_router(pesquisa_router.router, tags=["Pesquisa"])
 app.include_router(nps_router.router, tags=["Nps"])
 app.include_router(analisar_router.router, tags=["Analisar"])
+app.include_router(palavra_chave_router.router, tags=["Palavra Chave"])
--- scripts/model/palavra_chave.py
+++ scripts/model/palavra_chave.py
@@ -3,13 +3,14 @@ class PalavraChave:
     def __init__(self):
         pass
 
-    def __init__(self, id, score, texto, inicio, fim, id_pesquisa):
+    def __init__(self, id, score, texto, inicio, fim, id_pesquisa, classificacao_nps):
         self.id = id
         self.score = score
         self.texto = texto
         self.inicio = inicio
         self.fim = fim
         self.id_pesquisa = id_pesquisa
+        self.classificacao_nps = classificacao_nps
 
     @property
     def id(self):
@@ -60,5 +61,27 @@ class PalavraChave:
     def id_pesquisa(self, id_pesquisa):
         self.__id_pesquisa = id_pesquisa
 
+    @property
+    def classificacao_nps(self):
+        return self.__classificacao_nps
+    
+    @classificacao_nps.setter
+    def classificacao_nps(self, classificacao_nps):
+        self.__classificacao_nps = classificacao_nps
+
     def converter_in_comprehend(result, id_pesquisa):
         return PalavraChave(None, result["Score"], result["Text"], result["BeginOffset"], result["EndOffset"], id_pesquisa)
+
+    def builder(row):
+        return PalavraChave(row[0], row[1], row[2], row[3], row[4], row[5], row[6])
+
+    def json(self):
+        return {
+            "id": self.id,
+            "score": self.score,
+            "texto": self.texto,
+            "inicio": self.inicio,
+            "fim": self.fim,
+            "id_pesquisa": self.id_pesquisa,
+            "classificacao_nps": self.classificacao_nps
+        }
