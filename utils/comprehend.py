701515abb80b019b93a902827f6db49d5dc5b2f5
guhdalla <guhdalla011@gmail.com>
2022-08-06 15:32:47 -0300

    Refactor: Boto3 Client

--- utils/comprehend.py
+++ utils/comprehend.py
@@ -1,9 +1,14 @@
+from os import getenv
 import boto3
 
 class Comprehend:
 
     def __init__(self):
-        self.client = boto3.client('comprehend')
+        self.client = boto3.client('comprehend',
+        aws_access_key_id=getenv("ACCESS_KEY"),
+        aws_secret_access_key=getenv("SECRET_KEY"),
+        aws_session_token=getenv("SESSION_TOKEN")
+        )
 
     def detectar_sentimento(self, texto):
         result = self.client.detect_sentiment(Text=texto, LanguageCode='en')
