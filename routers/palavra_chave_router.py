b1c6bce8eeaf1bbf0d24d385b9202233f454fef2
guhdalla <guhdalla011@gmail.com>
2022-08-06 16:56:38 -0300

    Feat: Response api

--- routers/palavra_chave_router.py
+++ routers/palavra_chave_router.py
@@ -1,10 +1,20 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from service.palavra_chave_service import PalavraChaveService
+from pydantic import BaseModel
 
 router = APIRouter()
 
 service = PalavraChaveService()
 
-@router.get("/nuvemPalavras")
+class PalavraChave(BaseModel):
+    id: int
+    score: float
+    texto: str
+    inicio: int
+    fim: int
+    id_pesquisa: int
+    classificacao_nps: str
+
+@router.get("/nuvemPalavras", response_model=PalavraChave)
 def nuvem_palavras(size: int = 10, offset: int = 0, filtro: str = None):
     return service.nuvem_palavras(size, offset, filtro)
--- routers/pesquisa_router.py
+++ routers/pesquisa_router.py
@@ -1,10 +1,22 @@
+from datetime import date
+from urllib import response
 from fastapi import APIRouter, Depends, status, HTTPException
 from service.pesquisa_service import PesquisaService
+from pydantic import BaseModel
 
 router = APIRouter()
 
 service = PesquisaService()
 
-@router.get("/pesquisa")
+class Pesquisa(BaseModel):
+    id: int
+    texto: str
+    data: date
+    classificacao_nps: str
+    valor_nps: float
+    foi_analisado: bool
+    sentimento: str
+
+@router.get("/pesquisa", response_model=Pesquisa)
 def listar(size: int = 10, offset: int = 0, sentimento: str = None, nps: str = None, palavraChave: str = None):
     return service.listar(size, offset, palavraChave, sentimento, nps)
